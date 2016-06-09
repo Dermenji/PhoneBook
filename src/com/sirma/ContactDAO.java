eb10c1f4c53ee555adcaf230e0c25abec43c131b
Dermenji <alex.dermenji@gmail.com>
2016-06-09 23:49:33 +0300

    new data

--- src/com/sirma/ContactDAO.java
+++ src/com/sirma/ContactDAO.java
@@ -3,22 +3,23 @@ package com.sirma;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * Created by Alex on 6/8/2016.
- */
 public interface ContactDAO {
-    // Добавление контакта - возвращает ID добавленного контакта
-    public Long addContact(Contact contact) throws IOException;
-    // Удаление контакта по его ID
-    public void deleteContact(Long contactId);
-    public Contact getContact(Long contactId);
-    // Получение списка контактов
-    public List<Contact> showContacts();
 
-    public void loadFile();
-    public void syncData();
+    Long addContact(Contact contact) throws IOException;
 
-    public boolean isNameExist(String name);
+    void deleteContact(Long contactId);
 
-    public boolean isNumberExist(String number);
+    Contact getContact(Long contactId);
+
+    List<Contact> showContacts();
+
+    void loadFile();
+
+    void syncData();
+
+    boolean isNameExist(String name);
+
+    boolean isNumberExist(String number);
+
+    void exportToNewCSVfile(String newFile);
 }
