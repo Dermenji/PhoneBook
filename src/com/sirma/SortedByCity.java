015dc37bd86d21ef33b891017e72ce45152b1b31
Dermenji <alex.dermenji@gmail.com>
2016-06-11 00:48:18 +0300

    Sorting implementation

---
+++ src/com/sirma/SortedByCity.java
@@ -0,0 +1,16 @@
+package com.sirma;
+
+import java.util.Comparator;
+
+/**
+ * Created by fns02 on 6/10/2016.
+ */
+public class SortedByCity implements Comparator<Contact> {
+    @Override
+    public int compare(Contact o1, Contact o2) {
+        String str1 = o1.getPhone();
+        String str2 = o2.getPhone();
+
+        return str1.compareTo(str2);
+    }
+}
