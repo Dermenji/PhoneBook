523ea2746f2e646f916d82f89f918292eaa70685
Dermenji <alex.dermenji@gmail.com>
2016-06-16 00:15:16 +0300

    sorting by city refactoring

--- src/com/sirma/phonebook/SortedByCity.java
+++ src/com/sirma/phonebook/SortedByCity.java
@@ -5,10 +5,7 @@ import java.util.Comparator;
 public class SortedByCity implements Comparator<Contact> {
     @Override
     public int compare(Contact o1, Contact o2) {
-        String str1 = o1.getCity();
-        String str2 = o2.getCity();
-
-        int compareCities = str1.compareTo(str2);
+        int compareCities = o1.getCity().compareTo(o2.getCity());
 
         if (compareCities == 0) compareCities = (int) (o1.getContactId() - o2.getContactId());
         return compareCities;
