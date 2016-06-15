01777a5b0b67d4e794ed6e0448b7663f81f8dd3b
Dermenji <alex.dermenji@gmail.com>
2016-06-16 00:09:08 +0300

    sorting by phone refactoring

--- src/com/sirma/phonebook/SortedByPhone.java
+++ src/com/sirma/phonebook/SortedByPhone.java
@@ -6,15 +6,6 @@ import java.util.Comparator;
 public class SortedByPhone implements Comparator<Contact> {
     @Override
     public int compare(Contact o1, Contact o2) {
-        long phone1 = o1.getPhone();
-        long phone2 = o2.getPhone();
-
-        if (phone1 > phone2) {
-            return 1;
-        } else if (phone1 < phone2) {
-            return -1;
-        } else {
-            return 0;
-        }
+        return (int) (o1.getPhone() - o2.getPhone());
     }
 }
