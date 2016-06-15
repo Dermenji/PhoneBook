c87b1b7f2ae565f5f42568fbfe5d22f09683380c
Dermenji <alex.dermenji@gmail.com>
2016-06-16 00:09:38 +0300

    sorting by name refactoring

--- src/com/sirma/phonebook/SortedByName.java
+++ src/com/sirma/phonebook/SortedByName.java
@@ -5,9 +5,6 @@ import java.util.Comparator;
 public class SortedByName implements Comparator<Contact> {
     @Override
     public int compare(Contact o1, Contact o2) {
-        String str1 = o1.getName();
-        String str2 = o2.getName();
-
-        return str1.compareTo(str2);
+        return o1.getName().compareTo(o2.getName());
     }
 }
